0760fcbc9fc472e47cc7e1fa6fee9aafb5c7fc5f
GilangPambudi <2131710126@student.polinema.ac.id>
2024-08-14 13:01:03 +0700

    Handsout Pertemuan 23 - Blade View, Layout Web Template + AdminLTE.

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Yajra\DataTables\Facades\DataTables;
 use Illuminate\Support\Facades\DB;
-
 use Illuminate\Http\Request;
+use App\Models\LevelModel;
 
 class LevelController extends Controller
 {
@@ -20,7 +21,38 @@ class LevelController extends Controller
         // return "Delete data berhasil. Jumlah data yang dihapus: " . $row . " baris";
 
         //menampilkan data
-        $data = DB::select('select * from m_level');
-        return view('level', ['data' => $data]);
+        // $data = DB::select('select * from m_level');
+        // return view('level', ['data' => $data]);
+
+            $breadcrumb = (object)[
+                'title' => 'Daftar Level',
+                'list' => ['Home', 'Level']
+            ];
+    
+            $page = (object)[
+                'title' => 'Daftar level yang terdaftar dalam sistem'
+            ];
+    
+            $activeMenu = 'level'; //set menu yang sedang aktif
+    
+            return view('level', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+    }
+
+    public function list(Request $request)
+    {
+        $levels = LevelModel::select('level_id', 'level_kode', 'level_nama');
+
+        return DataTables::of($levels)
+            ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
+            ->addColumn('aksi', function ($level) { // menambahkan kolom aksi
+                $btn = '<a href="'.url('/level/' . $level->level_id).'" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="'.url('/level/' . $level->level_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="'.url('/level/'.$level->level_id).'">'
+                    . csrf_field() . method_field('DELETE') .
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
+                return $btn;
+            })
+            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
+            ->make(true);
     }
 }
--- resources/views/level.blade.php
+++ resources/views/level.blade.php
@@ -1,29 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.template')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-</head>
-<title>DB Facade</title>
+@section('content')
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools">
+                <a class="btn btn-sm btn-primary mt-1" href="{{ url('level/create') }}">Tambah</a>
+            </div>
+        </div>
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_level">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Kode Level</th>
+                        <th>Nama Level</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+@endsection
 
-<body>
-    <h1> data level pengguna</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th> ID</th>
-            <th> Kodel Level</th>
-            <th> nama level</th>
-        </tr>
-        @foreach ($data as $d)
-            <tr>
-                <td> {{ $d->level_id }}</td>
-                <td> {{ $d->level_kode }}</td>
-                <td> {{ $d->level_nama }}</td>
-            </tr>
-        @endforeach
-    </table>
-</body>
-
-</html>
+@push('js')
+    <script>
+        $(document).ready(function() {
+            var dataTable = $('#table_level').DataTable({
+                serverSide: true,
+                ajax: {
+                    "url": "{{ url('level/list') }}",
+                    "dataType": "json",
+                    "type": "POST",
+                },
+                columns: [
+                    { data: "DT_RowIndex", className: "text-center", orderable: false, searchable: false },
+                    { data: "level_kode", className: "", orderable: true, searchable: true },
+                    { data: "level_nama", className: "", orderable: true, searchable: true },
+                    { data: "aksi", className: "", orderable: false, searchable: false }
+                ]
+            });
+        });
+    </script>
+@endpush
